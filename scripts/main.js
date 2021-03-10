d0d9ab45699e1dc0d4c1fe6be6200249e18e85b2
Fernando Galdos <fergaldoss@gmail.com>
2021-03-09 23:23:50 -0500

    testing function with JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Outer Space";
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/space01.jpg") {
+    myImage.setAttribute("src", "images/space02.jpg");
+  } else {
+    myImage.setAttribute("src", "images/space01.jpg");
+  }
+};
