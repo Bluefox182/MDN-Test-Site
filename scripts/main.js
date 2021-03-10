cb15f45d1837b114bba45bc6174fccc5894d0ef0
Fernando Galdos <fergaldoss@gmail.com>
2021-03-09 23:47:48 -0500

    Adding a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function () {
     myImage.setAttribute("src", "images/space01.jpg");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Space is Awesome!, " + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Space is Awesome!, " + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
